f54e592ada9338d64406a204a28e56e90ec647ad
leo <none@none>
2008-08-17 01:39:45 +0200

    (no commit message)

--- Labyrinth.java
+++ Labyrinth.java
@@ -4,16 +4,14 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 /**
- * 
- */
-
-/**
  * @author leo
  *
  */
 public class Labyrinth {
 	private int width;
 	private int height;
+	private int holeTop;
+	private int holeBottom;
 	private static final int RUNTER=1;
 	private static final int RAUF=2;
 	private static final int RECHTS=4;
@@ -26,7 +24,35 @@ public class Labyrinth {
 	public Labyrinth(int width, int height) {
 		this.width=width;
 		this.height=height;
+		this.holeTop=(int) (Math.random()*(width-1));
+		this.holeBottom=(int) (Math.random()*(width-1));
 		grid=new int[width][height];
+	}
+	public String picString(int i) {
+		switch (i) {
+		case 0: return "0.png";
+
+		case RAUF: return "N.png";
+		case RUNTER: return "S.png";
+		case RECHTS: return "E.png";
+		case LINKS: return "W.png";
+
+		case RAUF | RUNTER: return "SN.png";
+		case RECHTS | LINKS: return "EW.png";
+		case RAUF | RECHTS: return "EN.png";
+		case RAUF | LINKS: return "WN.png";
+		case LINKS | RUNTER: return "WS.png";
+		case RECHTS | RUNTER: return "ES.png";
+
+		case RUNTER | RECHTS | LINKS: return "EWS.png";
+		case RAUF | RECHTS | LINKS: return "EWN.png";
+		case RAUF | RUNTER | LINKS: return "WSN.png";
+		case RAUF | RUNTER | RECHTS: return "ESN.png";
+
+		case RAUF | RUNTER | RECHTS | LINKS: return "EWSN.png";
+		default: return ""+i;
+		}
+
 	}
 	public String toString() {
 		String retVal = ("<br /><br />");
@@ -34,85 +60,75 @@ public class Labyrinth {
 		for(int i=0; i<height; i++) {
 			retVal+=("<tr>");
 			for(int j=0;j<width;j++) {
-				switch (grid[j][i]) {
-				case 0: retVal+=("<td></td>"); break;
-
-				case RAUF: retVal+=("<td>&#9474;</td>"); break;
-				case RUNTER: retVal+=("<td>&#9474;</td>"); break;
-				case RECHTS: retVal+=("<td>&#9472;</td>"); break;
-				case LINKS: retVal+=("<td>&#9472;</td>"); break;
-
-				case RAUF | RUNTER: retVal+=("<td>&#9474;</td>"); break;
-				case RECHTS | LINKS: retVal+=("<td>&#9472;</td>"); break;
-				case RAUF | RECHTS: retVal+=("<td>&#9492;</td>"); break;
-				case RAUF | LINKS: retVal+=("<td>&#9496;</td>"); break;
-				case LINKS | RUNTER: retVal+=("<td>&#9488;</td>"); break;
-				case RECHTS | RUNTER: retVal+=("<td>&#9484;</td>"); break;
-
-				case RUNTER | RECHTS | LINKS: retVal+=("<td>&#9516;</td>"); break;
-				case RAUF | RECHTS | LINKS: retVal+=("<td>&#9524;</td>"); break;
-				case RAUF | RUNTER | LINKS: retVal+=("<td>&#9508;</td>"); break;
-				case RAUF | RUNTER | RECHTS: retVal+=("<td>&#9500;</td>"); break;
-
-				case RAUF | RUNTER | RECHTS | LINKS: retVal+=("<td>&#9532;</td>"); break;
-
-				case NOTFREE | 0: retVal+=("<td style=color:#FF0000>0</td>"); break;
-
-				case NOTFREE | RAUF: retVal+=("<td style=color:#FF0000>&#9474;</td>"); break;
-				case NOTFREE | RUNTER: retVal+=("<td style=color:#FF0000>&#9474;</td>"); break;
-				case NOTFREE | RECHTS: retVal+=("<td style=color:#FF0000>&#9472;</td>"); break;
-				case NOTFREE | LINKS: retVal+=("<td style=color:#FF0000>&#9472;</td>"); break;
-
-				case NOTFREE | RAUF | RUNTER: retVal+=("<td style=color:#FF0000>&#9474;</td>"); break;
-				case NOTFREE | RECHTS | LINKS: retVal+=("<td style=color:#FF0000>&#9472;</td>"); break;
-				case NOTFREE | RAUF | RECHTS: retVal+=("<td style=color:#FF0000>&#9492;</td>"); break;
-				case NOTFREE | RAUF | LINKS: retVal+=("<td style=color:#FF0000>&#9496;</td>"); break;
-				case NOTFREE | LINKS | RUNTER: retVal+=("<td style=color:#FF0000>&#9488;</td>"); break;
-				case NOTFREE | RECHTS | RUNTER: retVal+=("<td style=color:#FF0000>&#9484;</td>"); break;
-
-				case NOTFREE | RUNTER | RECHTS | LINKS: retVal+=("<td style=color:#FF0000>&#9516;</td>"); break;
-				case NOTFREE | RAUF | RECHTS | LINKS: retVal+=("<td style=color:#FF0000>&#9524;</td>"); break;
-				case NOTFREE | RAUF | RUNTER | LINKS: retVal+=("<td style=color:#FF0000>&#9508;</td>"); break;
-				case NOTFREE | RAUF | RUNTER | RECHTS: retVal+=("<td style=color:#FF0000>&#9500;</td>"); break;
-
-				case NOTFREE | RAUF | RUNTER | RECHTS | LINKS: retVal+=("<td style=color:#FF0000>&#9532;</td>"); break;
-
-				default: retVal+=(grid[i*width+j]);
+				retVal+="<td";
+				if(debug) {
+					String style="background-color:";
+					if(0!=(grid[j][i] & NOTFREE))
+						style+="#ff0";
+					else
+						style+="#0ff";
+					retVal+=" style=\""+style+"\"";
 				}
+				retVal+="><img src=\"data/";
+				//es interessiert nur die richtung:
+				int toDisplay=grid[j][i]&(RAUF | RUNTER | RECHTS | LINKS);
+				if(j==0)
+					toDisplay^=LINKS;
+				if(j==width-1)
+					toDisplay^=RECHTS;
+				if(i==0) {
+					toDisplay^=RAUF;
+					if(j==holeTop)
+						toDisplay^=RECHTS;
+					if(j==holeTop+1)
+						toDisplay^=LINKS;
+				}
+				if(i==height-1) {
+					toDisplay^=RUNTER;
+					if(j==holeBottom)
+						toDisplay^=RECHTS;
+					if(j==holeBottom+1)
+						toDisplay^=LINKS;
+				}
+				retVal+=picString(toDisplay)+"\" /></td>";
 			}
 			retVal+=("</tr>");
 		}
 		retVal+=("</table>");
 		return retVal;
 	}
-	String getGrid() {
+	public String getGrid() {
 		String retVal="";
 		int freeNodeCount=0;
-		///Knoten, die noch wachsen können
+		///Knoten, die noch wachsen können. Array maximal großzügig dimensioniert.
 		int [] freeNode = new int[width*height];
 		boolean newline = true;
-		for (int i=0; i<width; i++ ) { //leeres grid innen
+		//init
+		//leeres grid innen
+		for (int i=0; i<width; i++ ) {
 			for (int j=0; j<height;j++) {
 				grid[i][j]=0;
 			}
 		}
-		for (int i=1;i<width-1 ;i++ ) {//oben und unten wand
+		//oben und unten wand. nach innen offen
+		for (int i=1;i<width-1 ;i++ ) {
 			grid[i][0]       = LINKS | RECHTS | RAUF;
 			grid[i][height-1]= LINKS | RECHTS | RUNTER;
 			freeNode[freeNodeCount++]= i;
 			freeNode[freeNodeCount++]=(height-1)*width+i;
 		}
-		for (int j=1;j<height-1 ;j++ ) {//rechts und links wand
+		//rechts und links wand. nach innen offen
+		for (int j=1;j<height-1 ;j++ ) {
 			grid[0][j]       = LINKS | RUNTER | RAUF;
 			grid[width-1][j] = RAUF | RECHTS | RUNTER;
 			freeNode[freeNodeCount++]=width*j;
 			freeNode[freeNodeCount++]=width*j+width-1;
 		}
 		//in den ecken wand
-		grid[0      ][0       ]=RECHTS | RUNTER;
-		grid[width-1][0       ]=LINKS  | RUNTER;
-		grid[0      ][height-1]=RECHTS | RAUF;
-		grid[width-1][height-1]=LINKS  | RAUF;
+		grid[0      ][0       ]=
+		grid[width-1][0       ]=
+		grid[0      ][height-1]=
+		grid[width-1][height-1]=RECHTS | RUNTER | LINKS  | RAUF;
 
 		while (freeNodeCount>0) {
 			int freenodeid=(int) (Math.random()*freeNodeCount);
@@ -178,13 +194,14 @@ public class Labyrinth {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Labyrinth l=new Labyrinth(7,5);
+		Labyrinth l=new Labyrinth(15,9);
 		File f=new File("test.html");
 		try {
 			FileOutputStream fos=new FileOutputStream(f);
 			fos.write(("<html><body><h1>Labyrinth("+l.width+","+l.height+")</h1>").getBytes());
 			fos.write(l.getGrid().getBytes());
 			fos.write("</body></html>".getBytes());
+			fos.close();
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
