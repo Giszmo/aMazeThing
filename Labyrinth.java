4676a2dbf13b763c90edf1e54d57a6e0f115a209
leo <none@none>
2008-08-16 23:34:29 +0200

    (no commit message)

--- Labyrinth.java
+++ Labyrinth.java
@@ -1,3 +1,8 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 /**
  * 
  */
@@ -14,62 +19,76 @@ public class Labyrinth {
 	private static final int RECHTS=4;
 	private static final int LINKS=8;
 	private static final int NOTFREE=16;
+	private static final boolean debug = false;
 
 	private int [][] grid;
-	
+
 	public Labyrinth(int width, int height) {
 		this.width=width;
 		this.height=height;
 		grid=new int[width][height];
 	}
-	public void printgrid() {
-		System.out.println("<table border=0 cellpadding=0 cellspacing=0>");
+	public String toString() {
+		String retVal = ("<br /><br />");
+		retVal+=("<table border=0 cellpadding=0 cellspacing=0>");
 		for(int i=0; i<height; i++) {
-			System.out.println("<tr>");
+			retVal+=("<tr>");
 			for(int j=0;j<width;j++) {
 				switch (grid[j][i]) {
-					case 0: System.out.println("<td></td>"); break;
-					case RAUF | RUNTER: System.out.println("<td>&#9474;</td>"); break;
-					case RAUF: System.out.println("<td>&#9474;</td>"); break;
-					case RUNTER: System.out.println("<td>&#9474;</td>"); break;
-					case RECHTS: System.out.println("<td>&#9472;</td>"); break;
-					case LINKS: System.out.println("<td>&#9472;</td>"); break;
-					case RECHTS | LINKS: System.out.println("<td>&#9472;</td>"); break;
-					case RAUF | RECHTS: System.out.println("<td>&#9492;</td>"); break;
-					case RAUF | LINKS: System.out.println("<td>&#9496;</td>"); break;
-					case LINKS | RUNTER: System.out.println("<td>&#9488;</td>"); break;
-					case RECHTS | RUNTER: System.out.println("<td>&#9484;</td>"); break;
-					case RUNTER | RECHTS | LINKS: System.out.println("<td>&#9516;</td>"); break;
-					case RAUF | RECHTS | LINKS: System.out.println("<td>&#9524;</td>"); break;
-					case RAUF | RUNTER | LINKS: System.out.println("<td>&#9508;</td>"); break;
-					case RAUF | RUNTER | RECHTS: System.out.println("<td>&#9500;</td>"); break;
-					case RAUF | RUNTER | RECHTS | LINKS: System.out.println("<td>&#9532;</td>"); break;
-					case NOTFREE | 0: System.out.println("<td style=color:#FF0000>0</td>"); break;
-					case NOTFREE | RAUF | RUNTER: System.out.println("<td style=color:#FF0000>&#9474;</td>"); break;
-					case NOTFREE | RAUF: System.out.println("<td style=color:#FF0000>&#9474;</td>"); break;
-					case NOTFREE | RUNTER: System.out.println("<td style=color:#FF0000>&#9474;</td>"); break;
-					case NOTFREE | RECHTS: System.out.println("<td style=color:#FF0000>&#9472;</td>"); break;
-					case NOTFREE | LINKS: System.out.println("<td style=color:#FF0000>&#9472;</td>"); break;
-					case NOTFREE | RECHTS | LINKS: System.out.println("<td style=color:#FF0000>&#9472;</td>"); break;
-					case NOTFREE | RAUF | RECHTS: System.out.println("<td style=color:#FF0000>&#9492;</td>"); break;
-					case NOTFREE | RAUF | LINKS: System.out.println("<td style=color:#FF0000>&#9496;</td>"); break;
-					case NOTFREE | LINKS | RUNTER: System.out.println("<td style=color:#FF0000>&#9488;</td>"); break;
-					case NOTFREE | RECHTS | RUNTER: System.out.println("<td style=color:#FF0000>&#9484;</td>"); break;
-					case NOTFREE | RUNTER | RECHTS | LINKS: System.out.println("<td style=color:#FF0000>&#9516;</td>"); break;
-					case NOTFREE | RAUF | RECHTS | LINKS: System.out.println("<td style=color:#FF0000>&#9524;</td>"); break;
-					case NOTFREE | RAUF | RUNTER | LINKS: System.out.println("<td style=color:#FF0000>&#9508;</td>"); break;
-					case NOTFREE | RAUF | RUNTER | RECHTS: System.out.println("<td style=color:#FF0000>&#9500;</td>"); break;
-					case NOTFREE | RAUF | RUNTER | RECHTS | LINKS: System.out.println("<td style=color:#FF0000>&#9532;</td>"); break;
-					default: System.out.println(grid[i*width+j]);
+				case 0: retVal+=("<td></td>"); break;
+
+				case RAUF: retVal+=("<td>&#9474;</td>"); break;
+				case RUNTER: retVal+=("<td>&#9474;</td>"); break;
+				case RECHTS: retVal+=("<td>&#9472;</td>"); break;
+				case LINKS: retVal+=("<td>&#9472;</td>"); break;
+
+				case RAUF | RUNTER: retVal+=("<td>&#9474;</td>"); break;
+				case RECHTS | LINKS: retVal+=("<td>&#9472;</td>"); break;
+				case RAUF | RECHTS: retVal+=("<td>&#9492;</td>"); break;
+				case RAUF | LINKS: retVal+=("<td>&#9496;</td>"); break;
+				case LINKS | RUNTER: retVal+=("<td>&#9488;</td>"); break;
+				case RECHTS | RUNTER: retVal+=("<td>&#9484;</td>"); break;
+
+				case RUNTER | RECHTS | LINKS: retVal+=("<td>&#9516;</td>"); break;
+				case RAUF | RECHTS | LINKS: retVal+=("<td>&#9524;</td>"); break;
+				case RAUF | RUNTER | LINKS: retVal+=("<td>&#9508;</td>"); break;
+				case RAUF | RUNTER | RECHTS: retVal+=("<td>&#9500;</td>"); break;
+
+				case RAUF | RUNTER | RECHTS | LINKS: retVal+=("<td>&#9532;</td>"); break;
+
+				case NOTFREE | 0: retVal+=("<td style=color:#FF0000>0</td>"); break;
+
+				case NOTFREE | RAUF: retVal+=("<td style=color:#FF0000>&#9474;</td>"); break;
+				case NOTFREE | RUNTER: retVal+=("<td style=color:#FF0000>&#9474;</td>"); break;
+				case NOTFREE | RECHTS: retVal+=("<td style=color:#FF0000>&#9472;</td>"); break;
+				case NOTFREE | LINKS: retVal+=("<td style=color:#FF0000>&#9472;</td>"); break;
+
+				case NOTFREE | RAUF | RUNTER: retVal+=("<td style=color:#FF0000>&#9474;</td>"); break;
+				case NOTFREE | RECHTS | LINKS: retVal+=("<td style=color:#FF0000>&#9472;</td>"); break;
+				case NOTFREE | RAUF | RECHTS: retVal+=("<td style=color:#FF0000>&#9492;</td>"); break;
+				case NOTFREE | RAUF | LINKS: retVal+=("<td style=color:#FF0000>&#9496;</td>"); break;
+				case NOTFREE | LINKS | RUNTER: retVal+=("<td style=color:#FF0000>&#9488;</td>"); break;
+				case NOTFREE | RECHTS | RUNTER: retVal+=("<td style=color:#FF0000>&#9484;</td>"); break;
+
+				case NOTFREE | RUNTER | RECHTS | LINKS: retVal+=("<td style=color:#FF0000>&#9516;</td>"); break;
+				case NOTFREE | RAUF | RECHTS | LINKS: retVal+=("<td style=color:#FF0000>&#9524;</td>"); break;
+				case NOTFREE | RAUF | RUNTER | LINKS: retVal+=("<td style=color:#FF0000>&#9508;</td>"); break;
+				case NOTFREE | RAUF | RUNTER | RECHTS: retVal+=("<td style=color:#FF0000>&#9500;</td>"); break;
+
+				case NOTFREE | RAUF | RUNTER | RECHTS | LINKS: retVal+=("<td style=color:#FF0000>&#9532;</td>"); break;
+
+				default: retVal+=(grid[i*width+j]);
 				}
 			}
-			System.out.println("</tr>");
+			retVal+=("</tr>");
 		}
-		System.out.println("</table>");
+		retVal+=("</table>");
+		return retVal;
 	}
-
-	void getGrid() {
+	String getGrid() {
+		String retVal="";
 		int freeNodeCount=0;
+		///Knoten, die noch wachsen können
 		int [] freeNode = new int[width*height];
 		boolean newline = true;
 		for (int i=0; i<width; i++ ) { //leeres grid innen
@@ -90,28 +109,17 @@ public class Labyrinth {
 			freeNode[freeNodeCount++]=width*j+width-1;
 		}
 		//in den ecken wand
-		grid[0][0]=
-		grid[width-1][0]=
-		grid[0][height-1]=
-		grid[width-1][height-1]=LINKS | RECHTS | RUNTER | RAUF;
-//		freeNode[freeNodeCount++]=0;
-//		freeNode[freeNodeCount++]=width-1;
-//		freeNode[freeNodeCount++]=width*(height-1);
-//		freeNode[freeNodeCount++]=width*(height-1)+width-1;
+		grid[0      ][0       ]=RECHTS | RUNTER;
+		grid[width-1][0       ]=LINKS  | RUNTER;
+		grid[0      ][height-1]=RECHTS | RAUF;
+		grid[width-1][height-1]=LINKS  | RAUF;
 
 		while (freeNodeCount>0) {
 			int freenodeid=(int) (Math.random()*freeNodeCount);
-//			int freenodeid=freenodecount-1;
 			int nodeId=freeNode[freenodeid];
 			int nodeX=freeNode[freenodeid]%width;
 			int nodeY=freeNode[freenodeid]/width;
 			int direction = (1 << (int)(Math.random()*4));
-	/*		if(!(tmpgrid[nodeid] & direction)) {
-				if(tmpgrid[nodeid+diffnachbar[direction]]==0) {
-					tmpgrid[nodeid] |= RAUF;
-					tmpgrid[nodeid-GridWidth] |= RUNTER;
-					freenode[freenodecount++]=nodeid-GridWidth;*/
-			//System.out.println("freenodeid: " << freenodeid << " tmpgrid[" << nodeid << "]: " << tmpgrid[nodeid] << " " << direction << "<br>";
 			switch (direction) {
 			//nicht rauf und oben noch frei
 			case RAUF:
@@ -149,35 +157,41 @@ public class Labyrinth {
 			}
 			//test, ob der aktuelle node noch wachsen kann.
 			if((0!=(grid[nodeX][nodeY] & RAUF)   || 0!=(grid[nodeX][nodeY-1])) &&
-			   (0!=(grid[nodeX][nodeY] & RUNTER) || 0!=(grid[nodeX][nodeY+1])) &&
-			   (0!=(grid[nodeX][nodeY] & LINKS)  || 0!=(grid[nodeX-1][nodeY])) &&
-			   (0!=(grid[nodeX][nodeY] & RECHTS) || 0!=(grid[nodeX+1][nodeY])) ) {
-//				System.out.println((freenode[freenodeid]%GridWidth) << "/" << (freenode[freenodeid]/GridWidth) << " ";
+					(0!=(grid[nodeX][nodeY] & RUNTER) || 0!=(grid[nodeX][nodeY+1])) &&
+					(0!=(grid[nodeX][nodeY] & LINKS)  || 0!=(grid[nodeX-1][nodeY])) &&
+					(0!=(grid[nodeX][nodeY] & RECHTS) || 0!=(grid[nodeX+1][nodeY])) ) {
+				//				retVal+=((freenode[freenodeid]%GridWidth) << "/" << (freenode[freenodeid]/GridWidth) << " ";
 				grid[nodeX][nodeY] |= NOTFREE;
 				freeNode[freenodeid]=freeNode[--freeNodeCount];
 				newline=true;
 			}
-//			newline=false;
-			if(newline) {
-				printgrid();
-				for(int i=0; i<freeNodeCount; i++) {
-					System.out.println((freeNode[i]%width) + "/" + (freeNode[i]/width) + "_");
-				}
-//				System.out.println();
+			if(newline && debug) {
+				retVal+=toString();
 				newline=false;
 			}
 		}
-		printgrid();
+		retVal+=toString();
+		return retVal;
 	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Labyrinth l=new Labyrinth(6,6);
-		System.out.println("<html><body><h1>Labyrinth("+l.width+","+l.height+")</h1>");
-		l.getGrid();
-		System.out.println("</body></html>");
+		Labyrinth l=new Labyrinth(7,5);
+		File f=new File("test.html");
+		try {
+			FileOutputStream fos=new FileOutputStream(f);
+			fos.write(("<html><body><h1>Labyrinth("+l.width+","+l.height+")</h1>").getBytes());
+			fos.write(l.getGrid().getBytes());
+			fos.write("</body></html>".getBytes());
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
